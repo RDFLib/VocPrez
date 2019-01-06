a58c7ace3f0e724692e6bdecbceaf74221445052
Edmond Chuc <edmond.chuc@csiro.au>
2019-01-07 09:34:42 +1000

    Added vocabularies published by CGI for GeoSciML.

--- data/source_RVA.py
+++ data/source_RVA.py
@@ -6,33 +6,217 @@ class RVA(Source):
     """Source for Research Vocabularies Australia
     """
     VOCAB_ENDPOINTS = {
-        'rva-50': {
-            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_geologic-unit-type_v0-1',
-            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/196/ga_geologic-unit-type_v0-1.ttl'
+        'rva-43': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_alteration-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/175/ga_alteration-type_v0-1.ttl'
+        },
+        'rva-124': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_borehole-drilling-method_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/373/ga_borehole-drilling-method_v0-1.ttl'
+        },
+        'rva-55': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_commodity-code_v0-2',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/211/ga_commodity-code_v0-2.ttl'
+        },
+        'rva-54': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_composition-category_v0-2',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/208/ga_composition-category_v0-2.ttl'
+        },
+        'rva-44': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_compound-material-constituent-part_v0-2',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/178/ga_compound-material-constituent-part_v0-2.ttl'
+        },
+        'rva-53': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_consolidation-degree_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/205/ga_consolidation-degree_v0-1.ttl'
         },
         'rva-52': {
             'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_contact-type_v0-1',
             'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/202/ga_contact-type_v0-1.ttl'
         },
+        'rva-51': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_convention-code-for-strike-and-dip-measurements_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/199/ga_convention-code-for-strike-and-dip-measurements_v0-1.ttl'
+        },
+        'rva-46': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_deformation-style_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/184/ga_deformation-style_v0-1.ttl'
+        },
+        'rva-49': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_description-purpose_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/193/ga_description-purpose_v0-1.ttl'
+        },
+        'rva-45': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_earth-resource-expression_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/181/ga_earth-resource-expression_v0-1.ttl'
+        },
+        'rva-81': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_earth-resource-form_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/294/ga_earth-resource-form_v0-1.ttl'
+        },
+        'rva-78': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_earth-resource-material-role_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/285/ga_earth-resource-material-role_v0-1.ttl'
+        },
+        'rva-82': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_earth-resource-shape_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/297/ga_earth-resource-shape_v0-1.ttl'
+        },
+        'rva-83': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_end-use-potential_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/300/ga_end-use-potential_v0-1.ttl'
+        },
+        'rva-120': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_environmental-impact_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/361/ga_environmental-impact_v0-1.ttl'
+        },
+        'rva-59': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_event-environment_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/223/ga_event-environment_v0-1.ttl'
+        },
+        'rva-58': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_event-process_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/220/ga_event-process_v0-1.ttl'
+        },
+        'rva-79': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_exploration-activity-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/288/ga_exploration-activity-type_v0-1.ttl'
+        },
+        'rva-77': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_exploration-result_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/282/ga_exploration-result_v0-1.ttl'
+        },
+        'rva-63': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_fault-movement-sense_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/240/ga_fault-movement-sense_v0-1.ttl'
+        },
+        'rva-66': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_fault-movement-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/249/ga_fault-movement-type_v0-1.ttl'
+        },
+        'rva-68': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_fault-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/255/ga_fault-type_v0-1.ttl'
+        },
+        'rva-123': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_foliation-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/370/ga_foliation-type_v0-1.ttl'
+        },
+        'rva-67': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_genetic-category_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/252/ga_genetic-category_v0-1.ttl'
+        },
+        'rva-121': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_geologic-unit-morphology_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/364/ga_geologic-unit-morphology_v0-1.ttl'
+        },
+        'rva-65': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_geologic-unit-part-role_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/246/ga_geologic-unit-part-role_v0-1.ttl'
+        },
+        'rva-50': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_geologic-unit-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/196/ga_geologic-unit-type_v0-1.ttl'
+        },
+        'rva-64': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_lineation-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/243/ga_lineation-type_v0-1.ttl'
+        },
+        'rva-224': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_mapping-frame-all-concepts_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/664/ga_mapping-frame-all-concepts_v0-1.ttl'
+        },
+        'rva-90': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_metamorphic-facies_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/321/ga_metamorphic-facies_v0-1.ttl'
+        },
+        'rva-91': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_metamorphic-grade_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/324/ga_metamorphic-grade_v0-1.ttl'
+        },
+        'rva-76': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_mineral-occurrence-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/279/ga_mineral-occurrence-type_v0-1.ttl'
+        },
+        'rva-125': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_mineral-resource-reporting-classification-method_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/376/ga_mineral-resource-reporting-classification-method_v0-1.ttl'
+        },
+        'rva-126': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_mine-status_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/379/ga_mine-status_v0-1.ttl'
+        },
+        'rva-75': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_mining-activity_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/276/ga_mining-activity_v0-1.ttl'
+        },
+        'rva-89': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_observation-method-mapped-feature_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/318/ga_observation-method-mapped-feature_v0-1.ttl'
+        },
+        'rva-60': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_orientation-determination-method_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/226/ga_orientation-determination-method_v0-1.ttl'
+        },
+        'rva-86': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_particle-aspect-ratio_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/309/ga_particle-aspect-ratio_v0-1.ttl'
+        },
+        'rva-87': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_particle-shape_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/312/ga_particle-shape_v0-1.ttl'
+        },
+        'rva-88': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_particle-type_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/315/ga_particle-type_v0-1.ttl'
+        },
+        'rva-183': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_planar-polarity-code_v1-0',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/512/ga_planar-polarity-code_v1-0.ttl'
+        },
+        'rva-74': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_processing-activity_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/273/ga_processing-activity_v0-1.ttl'
+        },
+        'rva-84': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_proportion-term_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/303/ga_proportion-term_v0-1.ttl'
+        },
+        'rva-73': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_raw-material-role_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/270/ga_raw-material-role_v0-1.ttl'
+        },
+        'rva-72': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_reserve-assessment-category_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/267/ga_reserve-assessment-category_v0-1.ttl'
+        },
+        'rva-71': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_resource-assessment-category_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/264/ga_resource-assessment-category_v0-1.ttl'
+        },
+        'rva-56': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_simple-lithology_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/214/ga_simple-lithology_v0-1.ttl'
+        },
         'rva-57': {
             'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_stratigraphic-rank_v0-1',
             'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/217/ga_stratigraphic-rank_v0-1.ttl'
         },
-        'rva-177': {
-            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_association-type_v1-2',
-            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/741/assoc.ttl'
+        'rva-70': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_unfc-code_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/261/ga_unfc-code_v0-1.ttl'
         },
-        'rva-178': {
-            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_feature-of-interest-type_v0-1',
-            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/491/ga_feature-of-interest-type_v0-1.ttl'
+        'rva-80': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_value-qualifier_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/291/ga_value-qualifier_v0-1.ttl'
         },
-        'rva-185': {
-            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_sample-type_v1-0',
-            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/518/ga_sample-type_v1-0.ttl'
+        'rva-85': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_vocabulary-relation_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/306/ga_vocabulary-relation_v0-1.ttl'
         },
-        'rva-186': {
-            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_ga-data-classification_v1-0',
-            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/521/ga_ga-data-classification_v1-0.ttl'
+        'rva-69': {
+            'sparql': 'http://vocabs.ands.org.au/repository/api/sparql/ga_waste-storage_v0-1',
+            'download': 'https://vocabs.ands.org.au/registry/api/resource/downloads/258/ga_waste-storage_v0-1.ttl'
         }
     }
 
