dabe691aaf1a4fffea52bc2d7dcb2a8d55c06bd3
Edmond Chuc <edmond.chuc@csiro.au>
2019-02-06 16:19:54 +1000

    Added helper function to extract concept names and make it a presentable title

--- helper.py
+++ helper.py
@@ -3,3 +3,9 @@ import urllib
 
 def url_encode(s):
     return urllib.parse.quote(s)
+
+
+def make_title(s):
+    result = s.split('#')[-1].split('/')[-1]
+    result = result.replace('_', ' ')
+    return result.title()
