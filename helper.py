7008099a90e6efe42bcdba45977b2231857bae5f
Edmond Chuc <edmond.chuc@csiro.au>
2019-02-21 16:37:11 +1000

    Made the make_title helper function more useful

--- helper.py
+++ helper.py
@@ -6,4 +6,15 @@ def url_encode(s):
 
 
 def make_title(s):
-    return ' '.join(s.split('#')[-1].split('/')[-1].split('_')).title()
+    # make title from URI
+    title = ' '.join(s.split('#')[-1].split('/')[-1].split('_')).title()
+
+    # replace dashes with whitespace
+    if '-' in title:
+        title = ' '.join(title.split('-')).title()
+
+    # replace periods with whitespace
+    if '.' in title:
+        title = ' '.join(title.split('.')).title()
+
+    return title
