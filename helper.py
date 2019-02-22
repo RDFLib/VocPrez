b905de38e3a262d3bd882f006e7c0f533c596549
Edmond Chuc <edmond.chuc@csiro.au>
2019-02-22 14:32:01 +1000

    Added template helper functions

--- helper.py
+++ helper.py
@@ -1,11 +1,14 @@
 import urllib
+import re
+from rdflib import URIRef
+import markdown
 
 
 def url_encode(s):
     try:
         return urllib.parse.quote(s)
     except:
-        print(s)
+        pass
 
 
 def make_title(s):
@@ -21,3 +24,49 @@ def make_title(s):
         title = ' '.join(title.split('.')).title()
 
     return title
+
+
+def parse_markdown(s):
+    return markdown.markdown(s)
+
+
+def is_email(email):
+    """
+    Check if the email is a valid email.
+    :param email: The email to be tested.
+    :return: True if the email matches the static regular expression, else false.
+    :rtype: bool
+    """
+    pattern = r"[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+    return True if re.search(pattern, email) is not None else False
+
+
+def strip_mailto(email):
+    return email[7:]
+
+
+def contains_mailto(email):
+    if email[:7] == 'mailto:':
+        return True
+    return False
+
+
+def is_url(url):
+    """
+    Check if the url is a valid url.
+    :param url: The url to be tested.
+    :type url: str
+    :return: True if the url passes the validation, else false.
+    :rtype: bool
+    """
+    if isinstance(url, URIRef):
+        return True
+
+    pattern = re.compile(
+        r'^(?:http|ftp)s?://'  # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'localhost|'  # localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+        r'(?::\d+)?'  # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return True if re.search(pattern, url) is not None else False
