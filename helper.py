43efdd1478129abc8b8cdd3a902f90860832aca7
Alex Ip <alex@trentham.net.au>
2019-09-16 13:29:24 +1000

    Stole Edmund Chuc's expandable treeview rendering code from VocView

--- helper.py
+++ helper.py
@@ -8,8 +8,27 @@ import pickle
 import logging
 import errno
 import _config as config
+from bs4 import BeautifulSoup
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def render_concept_tree(html_doc):
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    #concept_hierarchy = soup.find(id='concept-hierarchy')
+
+    uls = soup.find_all('ul')
+
+    for i, ul in enumerate(uls):
+        # Don't add HTML class nested to the first 'ul' found.
+        if not i == 0:
+            ul['class'] = 'nested'
+            if ul.parent.name == 'li':
+                temp = BeautifulSoup(str(ul.parent.a.extract()), 'html.parser')
+                ul.parent.insert(0, BeautifulSoup('<span class="caret">', 'html.parser'))
+                ul.parent.span.insert(0, temp)
+    return soup
 
 
 def url_encode(s):
