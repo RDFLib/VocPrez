fd4d4f554fcae4dba03a53070158717e8fd8ef87
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-08-10 21:48:54 +1000

    v1 search, nav moved to separate template

--- vocprez/app.py
+++ vocprez/app.py
@@ -451,6 +451,135 @@ def sparql():
     )
 
 
+@app.route("/search")
+def search():
+    if request.values.get("search"):
+        last_search = request.values.get("search")
+        if request.values.get("from") and request.values.get("from") != "all":
+            q = """
+                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    
+                SELECT DISTINCT ?uri ?pl (SUM(?weight) AS ?weight)
+                WHERE {{
+                    GRAPH <{grf}> {{
+                        {{  # exact match on a prefLabel always wins
+                            ?uri a skos:Concept ;
+                                 skos:prefLabel ?pl .
+                            BIND (50 AS ?weight)
+                            FILTER REGEX(?pl, "^{input}$", "i")
+                        }}
+                        UNION    
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:prefLabel ?pl .
+                            BIND (10 AS ?weight)
+                            FILTER REGEX(?pl, "{input}", "i")
+                        }}
+                        UNION
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:altLabel ?al ;
+                                 skos:prefLabel ?pl .
+                            BIND (5 AS ?weight)
+                            FILTER REGEX(?al, "{input}", "i")
+                        }}
+                        UNION
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:hiddenLabel ?hl ;
+                                 skos:prefLabel ?pl .
+                            BIND (5 AS ?weight)
+                            FILTER REGEX(?hl, "{input}", "i")
+                        }}        
+                        UNION
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:definition ?d ;
+                                 skos:prefLabel ?pl .
+                            BIND (1 AS ?weight)
+                            FILTER REGEX(?d, "{input}", "i")
+                        }}        
+                    }}
+                }}
+                GROUP BY ?uri ?pl
+                ORDER BY DESC(?weight) 
+                """.format(**{"grf": request.values.get("from"), "input": request.values.get("search")})
+            results = []
+        else:
+            q = """
+                PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+                SELECT DISTINCT ?g ?uri ?pl (SUM(?weight) AS ?weight)
+                WHERE {{
+                    GRAPH ?g {{
+                        {{  # exact match on a prefLabel always wins
+                            ?uri a skos:Concept ;
+                                 skos:prefLabel ?pl .
+                            BIND (50 AS ?weight)
+                            FILTER REGEX(?pl, "^{input}$", "i")
+                        }}
+                        UNION    
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:prefLabel ?pl .
+                            BIND (10 AS ?weight)
+                            FILTER REGEX(?pl, "{input}", "i")
+                        }}
+                        UNION
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:altLabel ?al ;
+                                 skos:prefLabel ?pl .
+                            BIND (5 AS ?weight)
+                            FILTER REGEX(?al, "{input}", "i")
+                        }}
+                        UNION
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:hiddenLabel ?hl ;
+                                 skos:prefLabel ?pl .
+                            BIND (5 AS ?weight)
+                            FILTER REGEX(?hl, "{input}", "i")
+                        }}        
+                        UNION
+                        {{
+                            ?uri a skos:Concept ;
+                                 skos:definition ?d ;
+                                 skos:prefLabel ?pl .
+                            BIND (1 AS ?weight)
+                            FILTER REGEX(?d, "{input}", "i")
+                        }}        
+                    }}
+                }}
+                GROUP BY ?g ?uri ?pl
+                ORDER BY DESC(?weight) 
+                """.format(**{"input": request.values.get("search")})
+            results = {}
+
+        for r in sparql_query(q):
+            if r.get("uri") is None:
+                break  # must do this check as r["weight"] will appear at least once with value 0 for no results
+            if request.values.get("from") and request.values.get("from") != "all":
+                results.append((r["uri"]["value"], r["pl"]["value"]))
+            else:
+                if r["g"]["value"] not in results.keys():
+                    results[r["g"]["value"]] = []
+                results[r["g"]["value"]].append((r["uri"]["value"], r["pl"]["value"]))
+
+        return render_template(
+            "search.html",
+            vocabs=[(v.uri, v.title) for k, v in g.VOCABS.items()],
+            last_search=last_search,
+            selected_vocab=request.values.get("from"),
+            results=results
+        )
+    else:
+        return render_template(
+            "search.html",
+            vocabs=[(v.uri, v.title) for k, v in g.VOCABS.items()]
+        )
+
+
 # the SPARQL endpoint under-the-hood
 @app.route("/endpoint", methods=["GET", "POST"])
 def endpoint():
