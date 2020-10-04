99296ffc4339ea6d9a29496106504b9741cf2627
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-10-04 13:10:09 +1000

    support for other_properties

--- vocprez/model/concept.py
+++ vocprez/model/concept.py
@@ -5,11 +5,20 @@ from rdflib import Graph, URIRef, Literal
 from rdflib.namespace import DCTERMS, RDF, RDFS, SKOS
 from vocprez.model.profiles import profile_skos
 import vocprez._config as config
+from typing import List
+from vocprez.model.property import Property
 
 
 class Concept:
     def __init__(
-        self, vocab_uri, uri, prefLabel, definition, related_instances, annotations=None
+        self,
+        vocab_uri,
+        uri,
+        prefLabel,
+        definition,
+        related_instances,
+        annotations=None,
+        other_properties: List[Property] = None
     ):
         self.vocab_uri = vocab_uri
         self.uri = uri
@@ -18,6 +27,8 @@ class Concept:
         self.related_instances = related_instances
         self.annotations = annotations
         self.agents = None
+
+        self.other_properties = other_properties
 
 
 class ConceptRenderer(Renderer):
@@ -78,6 +89,10 @@ class ConceptRenderer(Renderer):
                             URIRef(k),
                             URIRef(inst[0])  # only URIs for RDF, not prefLabels too
                         ))
+
+        if self.concept.other_properties is not None:
+            for prop in self.concept.other_properties:
+                g.add((c, URIRef(prop.uri), prop.value))
 
         # serialise in the appropriate RDF format
         if self.mediatype in ["application/rdf+json", "application/json"]:
