ebdeb6fd755d97fb1fee6bce9e27b873001a154b
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-02-12 20:54:19 +1000

    typo

--- vocprez/model/profiles.py
+++ vocprez/model/profiles.py
@@ -4,7 +4,7 @@ from pyldapi.renderer import Renderer
 profile_skos = Profile(
     "https://www.w3.org/TR/skos-reference/",
     label="SKOS",
-    comment="Simple Knowledge Organization System (SKOS)is a W3C-authored, common data model for sharing "
+    comment="Simple Knowledge Organization System (SKOS) is a W3C-authored, common data model for sharing "
     "and linking knowledge organization systems "
     "via the Web.",
     mediatypes=["text/html", "application/json"] + Renderer.RDF_MEDIA_TYPES,
